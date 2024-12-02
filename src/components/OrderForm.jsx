bfd4f59afa93e6646f73a913619489f9caa30179
Ezgi Uygun Öztuna <ezgiuygn@gmail.com>
2024-12-02 13:47:15 +0300

    Boyut & Hamur Seçimi

--- src/components/OrderForm.jsx
+++ src/components/OrderForm.jsx
@@ -1,8 +1,31 @@
 import { BrowserRouter as Router, Link } from 'react-router-dom';
-import React from 'react';
+import React, { useState } from 'react';
 import "./OrderForm.css"
 
 export default function OrderForm() {
+    const [pizzaBoyutu, setPizzaBoyutu] = useState(""); // Pizza boyutunu tutucak.
+    const [hamurKalınlığı, setHamurKalınlığı] = useState(""); // Hamur seçimi yapılacak.
+    const [error, setError] = useState(""); //Hata mesajını tutucak.
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (!pizzaBoyutu) {
+            setError("Lütfen pizza boyutunu seçiniz!");
+            return;
+        }
+
+        if (!hamurKalınlığı) {
+            setError("Lütfen hamur kalınlığını seçiniz!");
+            return;
+        }
+
+        setError("");
+        console.log("Pizza Boyutu:", pizzaBoyutu);
+        console.log("Hamur Kalınlığı:", hamurKalınlığı || "Seçim yapılmadı");
+
+        alert("Sipariş başarıyla oluşturuldu!");
+    };
+
 
     return (
         <>
@@ -28,6 +51,58 @@ export default function OrderForm() {
             <p className='desc'>
                 Frontent Dev olarak hala position:absolute kullanıyorsan bu çok acı pizza tam sana göre. Pizza, domates, peynir ve genellikle çeşitli diğer malzemelerle kaplanmış, daha sonra geleneksel olarak odun ateşinde bir fırında yüksek sıcaklıkta pişirilen, genellikle yuvarlak, düzleştirilmiş mayalı buğday bazlı hamurdan oluşan İtalyan kökenli lezzetli bir yemektir. . Küçük bir pizzaya bazen pizzetta denir.
             </p>
+
+            <form onSubmit={handleSubmit}>
+                <div className='boyut-secim'>
+                    <h2>Boyut Seç <span style={{ color: "red" }}>*</span></h2>
+                    <label>
+                        <input
+                            type="radio"
+                            value="Küçük"
+                            checked={pizzaBoyutu === "Küçük"}
+                            onChange={(e) => setPizzaBoyutu(e.target.value)}
+                        />
+                        Küçük
+                    </label>
+                    <label>
+                        <input
+                            type="radio"
+                            value="Orta"
+                            checked={pizzaBoyutu === "Orta"}
+                            onChange={(e) => setPizzaBoyutu(e.target.value)}
+                        />
+                        Orta
+                    </label>
+                    <label>
+                        <input
+                            type="radio"
+                            value="Büyük"
+                            checked={pizzaBoyutu === "Büyük"}
+                            onChange={(e) => setPizzaBoyutu(e.target.value)}
+                        />
+                        Büyük
+                    </label>
+                </div>
+
+                <div className='hamur-secim'>
+                    <h2>Hamur Seç<span style={{ color: "red" }}>*</span></h2>
+                    <select
+                        value={hamurKalınlığı}
+                        onChange={(e) => setHamurKalınlığı(e.target.value)}
+                    >
+                        <option value="">Hamur Kalınlığı</option>
+                        <option value="İnce">İnce</option>
+                        <option value="Orta">Orta</option>
+                        <option value="Kalın">Kalın</option>
+                    </select>
+                </div>
+
+                {error && <p style={{ color: "red" }}>{error}</p>}
+
+                {/* Form Gönderme 
+                <button type="submit">SİPARİŞ VER</button>*/}
+
+            </form>
 
         </>
     );
