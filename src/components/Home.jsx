358e6f8ca03d0e198759afb49bc9a5bfda61a312
Ezgi Uygun Öztuna <ezgiuygn@gmail.com>
2024-12-05 14:54:53 +0300

    Home_final

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,17 +3,21 @@ import "./Home.css";
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import Card from './Card';
 import CardPictures from './CardPictures';
+import Footer from './Footer.jsx';
 
 export default function Home() {
     return (
         <div className="image">
+            <div className='new-image'>
+                <p className='firsat'>fırsatı kaçırma</p>
+                <p className='paragraf-home'>KOD ACIKTIRIR PİZZA,DOYURUR</p>
+                <img className="home-logo" src='/Assets/Iteration-1-assets/home-logo.svg' />
+                <Link to="/order">
+                    <button className='button-home'>ACIKTIM</button>
+                </Link>
+            </div>
             <img className="home-banner" src='/Assets/Iteration-1-assets/home-banner.png' />
-            <img className="home-logo" src='/Assets/Iteration-1-assets/home-logo.svg' />
-            <p className='paragraf-home'>KOD ACIKTIRIR PİZZA,DOYURUR
-            </p>
-            <Link to="/order">
-                <button className='button-home'>ACIKTIM</button>
-            </Link>
+
 
             <div className='secenekler'>
 
@@ -47,47 +51,51 @@ export default function Home() {
                     <p>Gazlı İçecek</p>
                 </div>
             </div>
+            <div className='for-background-color'>
 
-            <Card />
+                <Card />
 
-            <p className='paket'>en çok paketlenen menüler</p>
+                <p className='paket'>en çok paketlenen menüler</p>
 
-            <h1 className='baslik-bir'>Acıktıran Kodlara Doyuran Lezzetler</h1>
+                <h1 className='baslik-bir'>Acıktıran Kodlara Doyuran Lezzetler</h1>
 
-            <div className='secenekler-eng'>
+                <div className='secenekler-eng'>
 
-                <div className='ramen'>
-                    <img src='Assets/Iteration-2-aseets/icons/1.svg' />
-                    <p>Ramen</p>
+                    <div className='ramen'>
+                        <img src='Assets/Iteration-2-aseets/icons/1.svg' />
+                        <p>Ramen</p>
+                    </div>
+
+                    <div className='pizza-eng'>
+                        <img src='Assets/Iteration-2-aseets/icons/2.svg' />
+                        <p>Pizza</p>
+                    </div>
+
+                    <div className='burger-eng'>
+                        <img src='Assets/Iteration-2-aseets/icons/3.svg' />
+                        <p>Burger</p>
+                    </div>
+
+                    <div className='frenchfries'>
+                        <img src='Assets/Iteration-2-aseets/icons/4.svg' />
+                        <p>French fries</p>
+                    </div>
+
+                    <div className='fastfood-eng'>
+                        <img src='Assets/Iteration-2-aseets/icons/5.svg' />
+                        <p>Fast food</p>
+                    </div>
+
+                    <div className='softdrinks'>
+                        <img src='Assets/Iteration-2-aseets/icons/6.svg' />
+                        <p>Soft drinks</p>
+                    </div>
                 </div>
 
-                <div className='pizza-eng'>
-                    <img src='Assets/Iteration-2-aseets/icons/2.svg' />
-                    <p>Pizza</p>
-                </div>
-
-                <div className='burger-eng'>
-                    <img src='Assets/Iteration-2-aseets/icons/3.svg' />
-                    <p>Burger</p>
-                </div>
-
-                <div className='frenchfries'>
-                    <img src='Assets/Iteration-2-aseets/icons/4.svg' />
-                    <p>French fries</p>
-                </div>
-
-                <div className='fastfood-eng'>
-                    <img src='Assets/Iteration-2-aseets/icons/5.svg' />
-                    <p>Fast food</p>
-                </div>
-
-                <div className='softdrinks'>
-                    <img src='Assets/Iteration-2-aseets/icons/6.svg' />
-                    <p>Soft drinks</p>
-                </div>
+                <CardPictures />
             </div>
 
-            <CardPictures />
+            <Footer />
 
         </div >
     );
