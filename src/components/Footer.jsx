4410183a5457255ccf15b298301a4d4c11bf03b9
Ezgi Uygun Öztuna <ezgiuygn@gmail.com>
2024-12-26 14:30:59 +0300

    image folder changing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,17 +8,17 @@ const Footer = () => {
             <div className='footer-first-col'>
                 <h1>Teknolojik Yemekler</h1>
                 <div className='location'>
-                    <img src="Assets/Iteration-2-aseets/footer/icons/icon-1.png" />
+                    <img src="/Iteration-2-aseets/footer/icons/icon-1.png" />
                     <p>341 Londonderry Road, Istanbul Türkiye</p>
                 </div>
 
                 <div className='email'>
-                    <img src="Assets/Iteration-2-aseets/footer/icons/icon-2.png" />
+                    <img src="/Iteration-2-aseets/footer/icons/icon-2.png" />
                     <p>aciktim@teknolojikyemekler.com</p>
                 </div>
 
                 <div className='phone'>
-                    <img src="Assets/Iteration-2-aseets/footer/icons/icon-3.png" />
+                    <img src="/Iteration-2-aseets/footer/icons/icon-3.png" />
                     <p>+90 216 123 45 67</p>
                 </div>
             </div>
@@ -36,19 +36,19 @@ const Footer = () => {
             <div className='footer-third-col'>
                 <h1>Instagram</h1>
                 <div className='instagram-grid'>
-                    <img src="Assets/Iteration-2-aseets/footer/insta/li-0.png" />
-                    <img src="Assets/Iteration-2-aseets/footer/insta/li-1.png" />
-                    <img src="Assets/Iteration-2-aseets/footer/insta/li-2.png" />
-                    <img src="Assets/Iteration-2-aseets/footer/insta/li-3.png" />
-                    <img src="Assets/Iteration-2-aseets/footer/insta/li-4.png" />
-                    <img src="Assets/Iteration-2-aseets/footer/insta/li-5.png" />
+                    <img src="/Iteration-2-aseets/footer/insta/li-0.png" />
+                    <img src="/Iteration-2-aseets/footer/insta/li-1.png" />
+                    <img src="/Iteration-2-aseets/footer/insta/li-2.png" />
+                    <img src="/Iteration-2-aseets/footer/insta/li-3.png" />
+                    <img src="/Iteration-2-aseets/footer/insta/li-4.png" />
+                    <img src="/Iteration-2-aseets/footer/insta/li-5.png" />
                 </div>
             </div>
             <hr className='footer-section-divider' />
 
             <div className='footer-last'>
                 <p>© 2023 Teknolojik Yemekler. </p>
-                <img src="Assets/Iteration-2-aseets/footer/insta/Twitter.svg" className='twitter' />
+                <img src="/Iteration-2-aseets/footer/insta/Twitter.svg" className='twitter' />
             </div>
         </div >
     )
